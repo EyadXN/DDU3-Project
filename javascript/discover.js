d11d740204be9c7e2c178a0c00b12416117c6088
Vinniehua <huahaokiet@hotmail.com>
2026-05-19 13:42:33 +0200

    changes

--- javascript/discover.js
+++ javascript/discover.js
@@ -1,18 +1,4 @@
 class Movies {
-  async getMovies() {
-    try {
-      const response = await fetch("../javascript/database.json");
-      const movieData = await response.json();
-
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-
-      console.log(movieData);
-    } catch (err) {
-      console.log("Failed to load movies:", err);
-    }
-  }
 
   displayMovies() {
     let movieContainer = document.getElementById("movie-container");
---
+++ javascript/frontend.js
@@ -0,0 +1,17 @@
+async function getMovies() {
+  try {
+    const response = await fetch("../javascript/database.json");
+    const movieData = await response.json();
+
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    console.log(movieData);
+  } catch (err) {
+    console.log("Failed to load movies:", err);
+  }
+}
+
+getMovies();
